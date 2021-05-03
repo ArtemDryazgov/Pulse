cdb2da9472c05a8b4ac01f0cabfcbb7389b4d607
Artem Dryazgov <artomdrazgov@gmail.com>
2021-05-03 17:38:51 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 /* Скрипт слайдера */
+
 $(document).ready(function () {
 	$('.carousel__inner').slick({
+		dots: true,
 		infinite: true,
-		speed: 1200,
-		/* autoplay: true, */
-		autoplaySpeed: 2000,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/right.svg"></button>',
-		dotsClass: 'slick-dots',
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.svg"></button>',
 		responsive: [
 			{
-				breakpoint: 992,
+				breakpoint: 991,
 				settings: {
 					arrows: false
 				}
